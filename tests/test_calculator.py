485f0e6182c9dad04f1462720705c27567267889
shallow0613 <2014610565@qq.com>
2026-08-20 12:05:51 +0800

    在 test_calculator.py 中添加 teset_login 函数以测试登录功能

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,11 @@
 from jsonpath import jsonpath
 import requests
+
+def teset_login():
+    resp=requests.post(url='http://43.133.227.52/api/login',
+                       json={'username':'tester',
+                             'password':'123456'}).json()
+    assert resp['code']==200
 if __name__=='__main__':
     resp=requests.post(url='http://43.133.227.52/api/login',
                        json={'username':'tester',
