c8dff8fe89d72dd4b4c076779a1fa14cbe7f8e8f
shallow0613 <2014610565@qq.com>
2026-08-20 10:13:29 +0800

    更新 calculator.py，添加请求处理和 token 提取功能；新增 CI/CD 工作流配置文件

--- calculator.py
+++ calculator.py
@@ -24,4 +24,14 @@ def divide(left, right):
     return left / right
 
 
+from jsonpath import jsonpath
+import requests
+if __name__=='__main__':
+    resp=requests.post(url='http://43.133.227.52/api/login',
+                       json={'username':'tester',
+                             'password':'123456'}).json()
+    token=jsonpath(resp,'$.token')
+    print(token)
+
+
 #test restore
